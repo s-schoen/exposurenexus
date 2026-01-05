46c29ef80ebbc612c5453a765cd58f627ab24a7a
Steffen Schön <steffen1995@gmail.com>
2026-01-05 21:41:03 +0100

    ui: render additional columns in findings table

--- apps/ui/src/components/finding-table/columns.tsx
+++ apps/ui/src/components/finding-table/columns.tsx
@@ -2,6 +2,10 @@ import type { ColumnDef } from "@tanstack/react-table"
 import { DataTableColumnHeader } from "@/components/data-table/column-header.tsx"
 import type { Finding } from "@openvlp/types/model/finding"
 import { SeverityBadge } from "@/components/severity-badge.tsx"
+import { formatFindingStatus } from "@/lib/format.ts"
+import { useQuery } from "@tanstack/react-query"
+import { createAssetByIDQueryOptions } from "@/api/asset.ts"
+import { Spinner } from "@/components/ui/spinner.tsx"
 
 export const columns: ColumnDef<Finding>[] = [
   {
@@ -18,5 +22,33 @@ export const columns: ColumnDef<Finding>[] = [
     cell: ({ row }) => {
       return <SeverityBadge severity={row.getValue("severity")} />
     }
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    cell: ({ row }) => {
+      return formatFindingStatus(row.getValue("status"))
+    }
+  },
+  {
+    accessorKey: "assetId",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Asset" />
+    ),
+    cell: ({ row }) => {
+      const asset = useQuery(
+        createAssetByIDQueryOptions(row.getValue("assetId"))
+      )
+
+      return <div>{asset.isPending ? <Spinner /> : asset.data?.name}</div>
+    }
+  },
+  {
+    accessorKey: "source",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Source" />
+    )
   }
 ]
