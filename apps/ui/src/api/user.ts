e64a5a8a527755106744da34c7a4db192a9a03ad
Steffen Schön <steffen1995@gmail.com>
2026-04-15 19:37:41 +0200

    ui: implement api client for use create and update

--- apps/ui/src/api/user.ts
+++ apps/ui/src/api/user.ts
@@ -1,5 +1,5 @@
 import { keepPreviousData } from "@tanstack/react-query"
-import type { User } from "@openvlp/types/model/user"
+import type { CreateUser, UpdateUser, User } from "@openvlp/types/model/user"
 import { env } from "@/env.ts"
 import {
   DEFAULT_QUERY_STALE_TIME,
@@ -27,6 +27,44 @@ async function getUserByID(id: string): Promise<User> {
   const response = await fetch(`${env.VITE_API_URL}/api/users/${id}`, {
     method: "GET",
     credentials: "include"
+  })
+
+  if (!response.ok) {
+    const error = await parseErrorReply(response)
+    console.error(error)
+    throw error
+  }
+
+  return parseObjectReply<User>(response)
+}
+
+export async function createUser(user: CreateUser): Promise<User> {
+  const response = await fetch(`${env.VITE_API_URL}/api/users`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+
+  if (!response.ok) {
+    const error = await parseErrorReply(response)
+    console.error(error)
+    throw error
+  }
+
+  return parseObjectReply<User>(response)
+}
+
+export async function updateUser(id: string, user: UpdateUser): Promise<User> {
+  const response = await fetch(`${env.VITE_API_URL}/api/users/${id}`, {
+    method: "PUT",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
   })
 
   if (!response.ok) {
--- packages/types/src/model/user.ts
+++ packages/types/src/model/user.ts
@@ -1,5 +1,21 @@
 import { z } from "zod/v4"
 
+export const createUserSchema = z.strictObject({
+  name: z.string().trim().min(1),
+  email: z.email(),
+  username: z.string().trim().min(1),
+  displayUsername: z.string().trim().min(1),
+  password: z.string().min(1)
+})
+
+export const updateUserSchema = z.strictObject({
+  name: z.string().trim().min(1),
+  email: z.email(),
+  displayUsername: z.string().trim().min(1),
+  image: z.string().nullable(),
+  password: z.string().min(1).optional()
+})
+
 export const userSchema = z.strictObject({
   id: z.string().nonempty(),
   name: z.string().nonempty(),
@@ -12,4 +28,6 @@ export const userSchema = z.strictObject({
   updatedAt: z.date()
 })
 
+export type CreateUser = z.infer<typeof createUserSchema>
+export type UpdateUser = z.infer<typeof updateUserSchema>
 export type User = z.infer<typeof userSchema>
